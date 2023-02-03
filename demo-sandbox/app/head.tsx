eb8f08e1212418889baa5b0ea44e0fdbaa4346ea
Jueun Grace Yun <graceyunn@gmail.com>
2023-02-03 18:58:56 -0500

    oops

--- demo-sandbox/app/head.tsx
+++ demo-sandbox/app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
       <meta name='description' content='3D Hover Animation' />
       <meta
         property='og:image'
-        content='https://og-joonie.vercel.app/og/RotatingText?description=A%203D%20Text%20Animation%20Component%20for%20React&italics=1'
+        content='https://og-joonie.vercel.app/api/og?token=737d522da1db18de596afcb63e5f1342a541745e9be620fcab5c5a1201dceb47&title=RotatingText&description=A%203D%20Text%20Animation%20Component%20for%20React&hideName=false&italics=1'
       />
       <link rel='icon' href='/favicon.ico' />
     </>
