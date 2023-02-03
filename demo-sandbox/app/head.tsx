54606560d37a7d28241768d954432f790b45346f
Jueun Grace Yun <graceyunn@gmail.com>
2023-02-03 18:52:43 -0500

    update og image

--- demo-sandbox/app/head.tsx
+++ demo-sandbox/app/head.tsx
@@ -4,6 +4,10 @@ export default function Head() {
       <title>Rotating Text Demo</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta name='description' content='3D Hover Animation' />
+      <meta
+        property='og:image'
+        content='https://og-joonie.vercel.app/og/RotatingText?description=A%203D%20Text%20Animation%20Component%20for%20React&italics=1'
+      />
       <link rel='icon' href='/favicon.ico' />
     </>
   )
